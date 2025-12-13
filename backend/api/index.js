c0a2cfc819249863da847f041bee6985b3c9a007
nigamanwita-6550 <nigamanwita@gmail.com>
2025-12-13 17:12:57 +0530

    changed index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,4 @@
 // api/index.js
 const serverless = require('serverless-http');
-const app = require('../api/index.js'); // import Express app
+const app = require('../server/index'); // import Express app
 module.exports = serverless(app);
