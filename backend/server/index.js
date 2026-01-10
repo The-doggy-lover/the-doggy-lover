0375c231bc3adaea108505b0d46a23104f221ad1
nigamanwita-6550 <nigamanwita@gmail.com>
2026-01-10 14:36:12 +0530

    changed coop

--- backend/server/index.js
+++ backend/server/index.js
@@ -44,8 +44,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
-    // Allow Google popup communication if necessary:
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+
   } else {
     // no Access-Control-Allow-Origin header set if not allowed
   }
