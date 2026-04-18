b563155324a8508472188d2b915b9adbf083ea56
nigamanwita-6550 <nigamanwita@gmail.com>
2026-04-18 16:14:11 +0530

    changed db.js

--- backend/server/lib/db.js
+++ backend/server/lib/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'pmpuser',
+  user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'Anni123#',
   database: process.env.DB_NAME || 'petmypet',
   waitForConnections: true,
